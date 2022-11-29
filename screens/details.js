73def7d9f50ee066e705cd42a8bb39869666d628
SparkxE <sylverblade16@gmail.com>
2022-11-28 19:24:13 -0600

    finally fixed the Details screen

--- screens/details.js
+++ screens/details.js
@@ -9,45 +9,58 @@ import { DataStore } from 'aws-amplify';
 import { Recipes } from '../src/models';
 import { useRoute } from '@react-navigation/native';
 
-const Details = () => {
+//let steps=[];
+
+export default function Details() {
   const route = useRoute();
-  //const [steps, setSteps] = useState([]);
-  //let subscription;
-  // useEffect(() => {
-  //   //query list and allow data updates
-  //   subscription = DataStore.observeQuery(Recipes, items.id("eq", route.params.id)).subscribe((snapshot) => {
-  //     const { items, isSynced } = snapshot;
-  //     console.log(items.id);
-  //     // setSteps();
-  //   });
+  const [steps, setSteps] = useState([]);
+  const test = [
+    {id: 1, text: "one"},
+    {id: 2, text: "two"}
+  ];
+  //let steps=[];
 
-  //   //unsubscribe to updates when component is destroyed to prevent memory leak
-  //   return function cleanup() {
-  //     subscription.unsubscribe();
-  //   }
-  // }, []);
+  useEffect(() => {
+    //query list and allow data updates
+    const subscription = DataStore.observeQuery(Recipes, items => items.id("eq", route.params.id)).subscribe((snapshot) => {
+      const { items, isSynced } = snapshot;
+      //steps.push(items[0].Details);
+      setSteps(items[0].Details);
+      // for(let i = 0; i<items[0].Details.length; i++){
+      //   steps.push(items[0].Details[i]);
+      // }
+    });
 
-  const renderItem = ({ item }) => {
+    //unsubscribe to updates when component is destroyed to prevent memory leak
+    return function cleanup() {
+      subscription.unsubscribe();
+    }
+  }, []);
 
-    <View style={styles.listArea}>
-      <Text>{item}</Text>
+  const renderItem  =({item, index}) =>(
+    <View>
+      <Text style = {styles.itemText}>{index+1}. {item}</Text>
     </View>
+  )
+
+  for(let i = 0; i<steps.length; i++){
+    console.log(steps[i]);
   }
 
   return (
     <View style={styles.listArea}>
       <Text>
         <Text style={styles.headerText}>
-          {route.params.name}
+          {route.params.name}{'\n\n'}
         </Text>
       </Text>
-      <Text>{""}</Text>
+      <View></View>
       <FlatList
-        data={route.params.steps}
-        renderItem={renderItem}
-      ></FlatList>
+          data={steps}
+          keyExtractor={(item, index)=>item.id}
+          renderItem={renderItem}
+          style={{flex: 1}}
+        ></FlatList>
     </View>
   );
 }
-
-export default Details;
